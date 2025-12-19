21ecd028c608ead9297c81bc6d349bb03c761c4a
kunweb04 <xohoxx0@gmail.com>
2025-12-19 11:16:56 +0800

    13

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https:/oho1417.com/",
+	site: "https://kunweb04.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
